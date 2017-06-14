24e3166bb11d3bd90222658bbfa5c58159f8238b
IndyJoeA <joe.alloway@gmail.com>
2017-06-14 17:03:20 -0400

    Read auth token from database

--- __init__.py
+++ __init__.py
@@ -6,17 +6,15 @@ import BlynkLib
 import logging
 import time
 
-# BLYNK_AUTH = 'd9db144199944107860c3399ac30b2bd'
-
-# Global Blynk object
-# blynk = BlynkLib.Blynk(BLYNK_AUTH)
+# Global Blynk objects
 blynk = None
+blynk_auth = None
 
 def blynkAuth():
-	blynk_auth = cbpi.get_config_parameter("blynk_auth_token", None)
+	global blynk	
 	if blynk_auth is None:
 		cbpi.notify("Blynk Error", "No Blynk token specified", type="danger", timeout=None)
-	else
+	else:				
 		blynk = BlynkLib.Blynk(blynk_auth)
 		start_new_thread(blynkConnection, ())
 
@@ -24,22 +22,24 @@ def blynkConnection():
 	blynk.run()
 
 def blynkDB():
-	print "INIT BLYNK DB"
-	with app.app_context():
-		db = get_db()
-
-		with app.open_resource('./plugins/BlynkPlugin/schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-
-		db.commit()
-	ConfigView.init_cache()
+	global blynk_auth	
+	blynk_auth = cbpi.get_config_parameter("blynk_auth_token", None)
+	if blynk_auth is None:
+		print "INIT BLYNK DB"
+		with app.app_context():
+			db = get_db()
+	
+			with app.open_resource('./plugins/BlynkPlugin/schema.sql', mode='r') as f:
+				db.cursor().executescript(f.read())
+	
+			db.commit()
+		ConfigView.init_cache()
 
 @cbpi.initalizer(order=8045)
 def init(cbpi):
 	cbpi.app.logger.info("INITIALIZE BLYNK PLUGIN")
 	blynkDB()
 	blynkAuth()
-	blynkConnection()
 
 @cbpi.backgroundtask(key="blynk_send_reading", interval=1)
 def blynk_send_reading():
@@ -49,4 +49,4 @@ def blynk_send_reading():
 		
 			# notify_text = 'id: %s val: %s' % (value.id, v)
 			# cbpi.notify("Sensor", notify_text, timeout=3)
-			# blynk.virtual_write(value.id, v)
+			blynk.virtual_write(value.id, v)
